57e326b85edd155dd35906bd0c4791aee72e4cfa
levirre <krischeval@outlook.com>
2020-07-17 19:45:41 -0700

    move templates folder. Start jinja home route

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "<h1 style='color:blue'>Imouto Squad</h1>"
+def index():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1')
+
