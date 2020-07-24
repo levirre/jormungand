d80a965cafb34bd231fd211e8807e28429f039ce
levirre <krischeval@outlook.com>
2020-07-23 19:34:14 -0700

    basic login form

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
+from database.db import db
+from flask import request
+
 app = Flask(__name__)
-print(__name__)
-@app.route('/')
-def index():
-    return render_template('home.html')
+
+
+#@app.route('/')
+#def index():
+#    return render_template('home.html')
+
+@app.route('/',methods=(['GET','POST']))
+def register():
+    if request.method =='POST':
+        username = request.form['user']
+        password = request.form['hash']
+        db(username,password)
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
+
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1')
--- flaskr/config.py
+++ flaskr/config.py
@@ -1,17 +1,35 @@
 #
+started = False;
+#import os
+#from dotenv import load_dotenv, find_dotenv
+while True:
+    c=input(">")
+    if c == "start":
+        if started==True:
+            print("car already started")
+        else:
+            started=True;
+            print ("car start")
+    elif c == "stop":
+        if started==False:
+            print("car already stopped")
+        else:
+            started=False;
+            print ("car stop")
+    else:
+        break;
 
-import os
-from dotenv import load_dotenv, find_dotenv
-
-
-class Config(object):
-    load_dotenv(find_dotenv())
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+#class Config(object):
+#    load_dotenv(find_dotenv())
+#    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+#    SQLALCHEMY_TRACK_MODIFICATIONS = False
 #engine = create_engine('postgresql+psycopg2://lev:1234@localhost:5433/lolidb')
 #print(engine.table_names())
 #load_dotenv(find_dotenv())
 #print(os.getenv("DATABASE_URL"))
 
 #TODO: connect config.py to __init__
-# print table names
+    # print table names
+    #how to create the local db in python
+    #connect db with app
+    #migrate db to heroku
