59e785dd8a95c8623df7795956effa1abb4f9209
levirre <krischeval@outlook.com>
2020-07-23 22:06:11 -0700

    change app instance

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,25 +1,24 @@
 from flask import Flask, render_template
 from database.db import db
 from flask import request
-
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__,instance_relative_config=True)
 
 
 #@app.route('/')
 #def index():
 #    return render_template('home.html')
 
-@app.route('/',methods=(['GET','POST']))
-def register():
-    if request.method =='POST':
-        username = request.form['user']
-        password = request.form['hash']
-        db(username,password)
-        return render_template('home.html')
-    else:
-        return render_template('login.html')
+    @app.route('/',methods=(['GET','POST']))
+    def register():
+        if request.method =='POST':
+            username = request.form['user']
+            password = request.form['hash']
+            db(username,password)
+            return render_template('home.html')
+        else:
+            return render_template('login.html')
 
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1')
+    return app
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
-from flaskr import app
+from flaskr import create_app
 
 if __name__ == "__main__":
+    app=create_app()
     app.run()
+
+    
